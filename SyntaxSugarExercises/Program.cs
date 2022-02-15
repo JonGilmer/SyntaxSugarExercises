4c2ef143c6af055c6a45c573d910672b9728d3e5
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-15 12:47:30 -0500

    Completed version of Syntax Sugar Exercise

--- SyntaxSugarExercises/Program.cs
+++ SyntaxSugarExercises/Program.cs
@@ -6,7 +6,23 @@ namespace SyntaxSugarExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ORIGINAL CODE:
+            //int answer = 4;
+            //string response;
+            //if (answer = 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + “greater than or equal to nine”;
+            //};
+
+
+            //Syntax Sugar Version
+            var answer = 4;
+
+            var response = (answer == 9) ? $"{answer} + is less than nine" : $"{answer} + greater than or equal to nine";
         }
     }
 }
